9ed7be136d028eb1b333d70cfe2ccaa02545f6eb
d11wtq <chris@w3style.co.uk>
2015-04-20 10:40:44 +0000

    Update to Docker 1.6.0, add docker-compose.

--- README.md
+++ README.md
@@ -4,15 +4,14 @@ This is a very minimal AMI that has Docker installed, on Ubuntu (HVM).
 
 It uses the official Ubuntu Docker (lxc-docker) packages.
 
-> Public: ami-9392faa9
-
 ## Usage
 
 This is a [Packer](https://packer.io/) template. Install the latest version of
 Packer, then:
 
     packer build \
-      -var docker_version=1.3.1 \
+      -var docker_version=1.6.0 \
+      -var source_ami=ami-d05e75b8 \
       -var aws_region=$AWS_REGION \
       -var aws_access_key=$AWS_ACCESS_KEY_ID \
       -var aws_secret_key=$AWS_SECRET_ACCESS_KEY \
--- provision.sh
+++ provision.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-sudo sh <<EOF
+sudo version=$docker_version sh <<'EOF'
+export DEBIAN_FRONTEND=noninteractive
+
+cmp_url=https://github.com/docker/compose/releases/download
+cmp_bin=docker-compose-Linux-x86_64
+cmp_ver=1.1.0
+
 sleep 30
+
+curl -L $cmp_url/$cmp_ver/$cmp_bin > /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
   --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
@@ -9,13 +19,10 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
 echo 'deb http://get.docker.io/ubuntu docker main' \
   > /etc/apt/sources.list.d/docker.list
 
-export DEBIAN_FRONTEND
-
-apt-get -y update && apt-get -y upgrade
-
-apt-get -y install \
-  lxc-docker-$DOCKER_VERSION \
-  linux-image-extra-$(uname -r)
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install linux-image-extra-$(uname -r)
+apt-get -y install lxc-docker-$version
 
 usermod -aG docker ubuntu
 EOF
